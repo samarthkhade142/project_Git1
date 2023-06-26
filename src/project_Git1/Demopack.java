d26fd2caa3f75fe62036802a2e3ebf2a0da42315
Khade Samarth <samarthkhade98@gmail.com>
2023-06-26 17:40:02 +0530

    New Demopack class created

---
+++ src/project_Git1/Demopack.java
@@ -0,0 +1,11 @@
+package project_Git1;
+
+public class Demopack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is demopack class");
+
+	}
+
+}
